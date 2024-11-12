aa0e3f1dd375a2fc52bea95efadd80751526347e
Justus446 <omondijustus056@gmail.com>
2024-11-12 09:31:47 +0300

    run table and storage from main

--- main.go
+++ main.go
@@ -6,10 +6,10 @@ func main() {
 
 	fmt.Println(("Hello, World!"))
 	todos := Todos{}
-	todos.add("Learn Go")
-	todos.add("Create a CLI App")
-	todos.add("Learn Go backend")
-	fmt.Printf("%+v\n", todos)
-	todos.delete(1)
-	fmt.Printf("%+v\n", todos)
+	storage := NewStorage[Todos]("todos.json")
+	fmt.Println("storage",storage)
+	storage.Load(&todos)
+	todos.toggle(1)
+	todos.print()
+	storage.Save(todos)
 }
